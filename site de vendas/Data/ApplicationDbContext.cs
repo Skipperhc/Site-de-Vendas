c00b2928bd7a9b8f296a93486d171e9f84ea2297
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2020-02-10 11:51:25 -0300

    site de vendas, base inicial

---
+++ site de vendas/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace site_de_vendas.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+    }
+}
